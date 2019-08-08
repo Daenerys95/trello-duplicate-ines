9e1788ba2bd9cefd86fc8626f6e4542dc64525ac
Ines Mouandjo Lobe <inesmouandjolobe@gmail.com>
2019-08-09 00:20:33 +0200

    bonus complété et méga bonus en cours

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var app = {
   init: function() {
     console.log('init');
-
-             
+    
+    $('#jeTriche').empty();     
+    
+    app.generateAddListButton();
     // ajout de la gestion de l'event click sur le bouton d'ajout de liste
     $('#addListButton').click(app.displayAddListModal);
 
@@ -19,11 +21,15 @@ var app = {
       });
     });
 
-     //intercepter évènement dblclick sur le nom des listes
-      $('.column h2').dblclick(app.changeListNameOnClick);  
+      
 
 
       $addListModalElement.find('form').submit(app.handleAddListFormSubmit);
+
+      app.listsAJAXRequest();
+      app.CardsAJAXRequest();
+
+
     
   },
 
@@ -53,11 +59,125 @@ var app = {
     
   },
 
-  generateListElement($listName){
+  generateListElement: function($listName, $listId){
     var $listTemplate = $('#listTemplate').contents().clone();
-    $listTemplate.find('.column h2').text($listName);
+    var $h2 = $listTemplate.find('.column h2').text($listName);
+    //intercepter évènement dblclick sur le nom des listes
+    $h2.dblclick(app.changeListNameOnClick); 
+    $listTemplate.attr('id', 'list_'+$listId);
     return $listTemplate;
   },
+
+  generateCardElement: function($cardName){
+    var $cardTemplate = $('#cardTemplate').contents().clone();
+    $cardTemplate.find('#cardName').text($cardName);
+    $cardTemplate.find('#dustbin').click(app.deleteCardOnClick);
+    $cardTemplate.find('#pencil').click(app.handleAddCardFormSubmit);
+    return $cardTemplate;
+  },
+
+  
+  handleAddCardFormSubmit: function(event) {
+    
+    var  $modal = $('#addListModal');
+    // Ajout de la classe active
+    $modal.addClass('is-active');
+    event.preventDefault();
+    console.log(event);
+    var $cardName = $(event.target).find('input[name="list-name"]').val();
+    var $newCard = app.generateCardElement($cardName);
+    if($cardName) {
+      $('#jeTriche').append($newCard);
+      $('#addListModal').removeClass('is-active');
+    }
+  },
+
+  listsAJAXRequest: function() {
+    $.ajax({
+      method: 'GET',
+      url:'data/lists.json',
+      dataType:'json',
+      data:'null'
+    })
+      .done(function(response){
+        //console.log(response);
+        app.lists = response;
+        for (var list of app.lists) {
+          var newLists = app.generateListElement(list.name, list.id);
+          $('#jeTriche').append(newLists);
+        }
+      })
+      .fail(function(error){
+        console.error(error);
+        alert('ta requête ne marche pas');
+      })
+  },
+
+  CardsAJAXRequest: function() {
+    $.ajax({
+      method: 'GET',
+      url:'data/cards.json',
+      dataType:'json',
+      data:'null'
+    })
+      .done(function(response){
+        //console.log(response);
+        app.cards = response;
+        for (var card of app.cards) {
+          //console.log(card);
+          var $newCard = app.generateCardElement(card.name);
+          app.addNewCardToNewList($newCard, card.list);
+         
+        }
+      })
+      .fail(function(error){
+        console.error(error);
+        alert('ta requête ne marche pas');
+      })
+  },
+
+  addNewCardToNewList: function(card, listId){
+    var $theList = $('#list_'+listId);
+    $theList.find('.panel-block').append(card);
+    
+  },
+
+  deleteCardOnClick: function (event) {
+    console.log('le click marche');
+    var deleteForm = app.createDeleteForm();
+    //$('.panel-block').append(deleteForm);
+  },
+
+  createDeleteForm() {
+    var $deleteFormTemplate = $('#deleteCard').contents().clone();
+    $deleteFormTemplate.find('.modal-card-title').text('Êtes-vous sûr de vouloir supprimer ?');
+    $deleteFormTemplate.find('.button .is-sucess').text('Confirmer');
+    $deleteFormTemplate.find('.button .close').text('Annuler');
+    $deleteFormTemplate.addClass('is-active');
+    console.log($deleteFormTemplate);
+    return $deleteFormTemplate;
+  },
+
+  generateAddListButton: function() {
+    // cette méthode va constuire le bouton "ajouter une liste"
+    // 1 - ajouter une div column
+    var columnDiv = $('<div>').addClass('column');
+    // 2 - ajouter un bouton dans la div column( avec les bonnes classes et id)
+    var button = $('<button>').addClass('button is-success').attr('id', 'addListButton');
+    button.appendTo(columnDiv);
+
+    // 3 - ajouter le contenu du bouton
+    var span = $('<span>').addClass('icon is-small').append(
+        $('<i>').addClass('fas fa-plus')
+    );
+    button.append(span);
+    button.append('&nbsp;Ajouter une liste');
+
+    // 4 - ajouter le tout au DOM
+    columnDiv.appendTo($('#jeTriche'));
+
+}
+
 };
 
 $(app.init);
